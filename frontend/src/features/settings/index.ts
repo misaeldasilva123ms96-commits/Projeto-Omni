daa8a0df99d69f6164f06209ad5fc2e63930ae5a
Misael Farias <misaeldasilva123.ms96@gmail.com>
2026-06-05 01:07:43 -0300

    fix(p5e-c): remove unresolved Settings UI files to restore CI build

--- frontend/src/features/settings/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './types';
-export { default as ProviderCard } from './ProviderCard';
-export { default as ProviderForm } from './ProviderForm';
-export { default as ProviderStatusBadge } from './ProviderStatusBadge';
